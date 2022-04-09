3d14ef357cacb2fad62a0a1d55f19ca33a35c140
Vera11113 <100492393+Vera11113@users.noreply.github.com>
2022-04-09 23:56:51 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,8 +0,0 @@
-revenue = int (input('Введите выручку компании:'))
-costs = int(input('Введите издержки компании:'))
-if revenue > costs:
-    print('Компания получила прибыль')
-elif revenue == costs:
-    print('Компания ничего не приобрела и не ушла в убыток')
-else:
-    print('У компании убыток')
